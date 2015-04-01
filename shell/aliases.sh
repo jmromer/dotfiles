8a061fc8e6aadcba175546e0213a805b295541a0
Jake Romer <jkrmr@github.com>
2015-03-31 23:09:55 -0700

    Augment buster_generate function

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -68,7 +68,17 @@ alias grep='GREP_COLOR="33;40" LANG=C grep --color=auto'
 #-------------------------------------------------------------
 # ALIASES: BUSTER
 #-------------------------------------------------------------
-alias bgd='buster generate && buster deploy'
+function buster_generate() {
+  if [ -d static ]; then
+    rm -rf ./static/
+    buster generate
+    cp ./source/* ./static/
+  else
+    echo 'Wrong directory: Must be in ghost project root'
+  fi
+}
+
+alias bgd='buster_generate && buster deploy'
 
 #-------------------------------------------------------------
 # ALIASES: RUBY, RAILS
