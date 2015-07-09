2b70b2ba7b10cf68ae92081e4e1cb657d05b04ae
Jake Romer <jkrmr@github.com>
2015-07-09 00:15:52 -0400

    Add emacs customizations

--- home/emacs.d/init.el
+++ home/emacs.d/init.el
@@ -41,4 +41,35 @@
 
 (ruby-mode)
 
-(add-to-list 'load-path "~/.emacs.d/custom")
+
+; Customization
+; =============
+
+;; Add a directory to our load path so that when you `load` things
+;; below, Emacs knows where to look for the corresponding file.
+(add-to-list 'load-path "~/.emacs.d/customizations")
+
+;; Sets up exec-path-from-shell so that Emacs will use the correct
+;; environment variables
+(load "shell-integration.el")
+
+;; These customizations make it easier for you to navigate files,
+;; switch buffers, and choose options from the minibuffer.
+(load "navigation.el")
+
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements
+(load "ui.el")
+
+;; These customizations make editing a bit nicer.
+(load "editing.el")
+
+;; Hard-to-categorize customizations
+(load "misc.el")
+
+;; For editing lisps
+(load "elisp-editing.el")
+
+;; Langauage-specific
+(load "setup-clojure.el")
+(load "setup-js.el")
--- home/lein/profiles.clj
+++ home/lein/profiles.clj
@@ -1,1 +1,2 @@
-{:user {:plugins [[lein-exec "0.3.1"]]}}
+{:user {:plugins [[lein-exec "0.3.1"]
+                  [cider/cider-nrepl "0.9.1"]]}}
