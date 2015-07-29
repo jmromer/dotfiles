eef92ac3d5ec2ceb665e8f5e1c88bcafe91fb7d4
Jake Romer <jkrmr@github.com>
2015-07-29 05:38:12 -0400

    Fix nrepl failing, add emacs layers

--- home/lein/profiles.clj
+++ home/lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[lein-exec "0.3.1"]
-                  [cider/cider-nrepl "0.9.1"]]}}
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0"]]}}
