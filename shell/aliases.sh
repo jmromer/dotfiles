23ffe8ac0a31ae98f8166aa88ba8ab12657d24dd
Jake Romer <jkrmr@github.com>
2015-08-15 22:01:20 -0400

    Shell: Remove unused aliases and functions

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,4 @@
 # shell/aliases.sh
-# NB: Disable alias by running with backslash, e.g.: \grep
 #-------------------------------------------------------------
 # ALIASES: Editors
 #-------------------------------------------------------------
@@ -7,20 +6,11 @@ alias vi='vim -u ~/.vim/minimal.vim'
 alias vimplug='vim +PlugUpdate +PlugClean! +qall'
 
 #-------------------------------------------------------------
-# ALIASES: Editors (note: postfix aliases unsupport in Bash)
-#-------------------------------------------------------------
-alias ln='ln -v'           # create links verbosely
-alias tlf='tail -f'        # watch file
-
-#-------------------------------------------------------------
 # ALIASES: FILE MANAGEMENT, SHELL NAVIGATION
 #-------------------------------------------------------------
 alias ..='\cd ..;ls'        # go to parent dir and list contents
 alias ...='\cd ../..;ls'    # go to grandparent dir and list contents
-alias _='\cd -;ls'          # go to previous wd and list contents
 alias mkdir='mkdir -p'      # create subdirectories as necessary
-alias desk='\cd ~/Desktop'  # go to desktop
-alias dev='\cd ~/Developer' # go to developer folder
 alias h='history'           # show history
 alias l='ls --color'        # list files (--color flag only works with GNU ls)
 alias la='ls -A'            # list all files
@@ -30,8 +20,8 @@ alias lla='ls -Aloh'        # all files, info, hide group, short sizes
 alias llag='ls -Al'         # all files, info, show group, short sizes
 alias l.='ls -d .*'         # show only files beginning with a dot
 alias ll.='ls -dloh .*'     # extended listing of files beginning with a dot
-alias d='dirs -v'           # show directory stack
 alias lt='tree'             # show directory as tree
+alias d='dirs -v'           # show directory stack
 alias c='clear'             # clear shell output
 alias q='exit'              # quit the current process
 
@@ -44,17 +34,12 @@ alias cp='cp -i'            # confirm copy if overwriting existing file
 alias ln='ln -iv'           # display error if link exists; link verbosely
 
 #-------------------------------------------------------------
-# ALIASES: MAC STUFF, MISC
+# ALIASES: MISC
 #-------------------------------------------------------------
 alias diff='colordiff'        # compare files, colorize output
 alias hide='setfile -a V'     # hide a file
 alias unhide='setfile -a v'   # unhide a file
-alias octave='octave -q'      # suppress greeting and version message
 alias v='vagrant'
-
-#-------------------------------------------------------------
-# ALIASES: NODE, NPM, GREP
-#-------------------------------------------------------------
 alias npmls='npm ls -depth=0'
 alias grep='GREP_COLOR="33;40" LANG=C grep --color=auto'
 
@@ -82,4 +67,3 @@ function heroku-reset() {
 function heroku-dropdb() {
   heroku pg:reset DATABASE --confirm "$1"
 }
-
