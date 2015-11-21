51d55e9fcdc01acd9252a20494c14ac001f0daac
Jake Romer <jkrmr@github.com>
2015-11-21 00:37:31 -0500

    Dotfiles: Omnibus update

--- laptop/gnu_tools.sh
+++ laptop/gnu_tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #-------------------------------------------------------------
-# Homebrewed GNU Tools and Vim
+# Homebrewed GNU Tools
 #-------------------------------------------------------------
 brew tap homebrew/dupes
 
--- laptop/homebrew_package_sets.sh
+++ laptop/homebrew_package_sets.sh
@@ -16,22 +16,18 @@ brew cask install haskell-platform
 # Vagrant
 brew cask install virtualbox vagrant
 
-# Docker
-brew cask install docker-compose
-
-# Python, Pip, Buster, NeoVim Python Client
+# Python, Pip, Buster
 pip install --upgrade pip setuptools
 pip install --upgrade buster
-pip install --upgrade neovim
 
 # Exercism
 brew tap homebrew/binary
 brew install exercism
 
 # Go
-brew install go
-mkdir $HOME/.go
-mkdir -p $HOME/.go/src/github.com/user
+# brew install go
+# mkdir $HOME/.go
+# mkdir -p $HOME/.go/src/github.com/user
 
 # R
 brew tap homebrew/science
@@ -39,5 +35,5 @@ brew install r
 brew cask install rstudio
 
 # Julia
-brew tap staticfloat/julia
-brew install julia
+# brew tap staticfloat/julia
+# brew install julia
