555f22346e68aceb6b714e54c5d66320e02acca8
Jake Romer <jkrmr@github.com>
2016-03-30 21:58:11 -0700

    Laptop: Disable peripheral packages installation
    TODO: Prompt for user approval

--- laptop/homebrew_package_sets.sh
+++ laptop/homebrew_package_sets.sh
@@ -1,39 +1,42 @@
 #-------------------------------------------------------------
 # Packages by language / framework
 #-------------------------------------------------------------
-# Scala
-brew install scala
-brew install sbt
-brew cask install scala-ide
-brew cask install intellij-idea-ce
 
-# Clojure
-brew install leiningen clojurescript
-
-# Haskell
-brew cask install haskell-platform
-
-# Vagrant
-brew cask install virtualbox vagrant
-
-# Python, Pip, Buster
-pip install --upgrade pip setuptools
-pip install --upgrade buster
-
-# Exercism
-brew tap homebrew/binary
-brew install exercism
-
-# Go
+# # Scala
+# brew install scala
+# brew install sbt
+#
+# # Clojure
+# brew install leiningen clojurescript
+#
+# # Haskell
+# brew cask install haskell-platform
+#
+# # Vagrant
+# brew cask install virtualbox vagrant
+#
+# # Python, Pip, Buster
+# pip install --upgrade pip setuptools
+# pip install --upgrade buster
+#
+# # Exercism
+# brew tap homebrew/binary
+# brew install exercism
+#
+# # Go
 # brew install go
 # mkdir $HOME/.go
 # mkdir -p $HOME/.go/src/github.com/user
-
-# R
-brew tap homebrew/science
-brew install r
-brew cask install rstudio
-
-# Julia
+#
+# # R
+# brew tap homebrew/science
+# brew install r
+# brew cask install rstudio
+#
+# # Julia
 # brew tap staticfloat/julia
 # brew install julia
+#
+# # OCaml
+# brew install opam
+# opam install merlin
