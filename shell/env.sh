65af8f005b5e32620910dadb16a21b0a676b227a
Jake Romer <jkrmr@github.com>
2016-05-31 20:07:13 -0400

    GPG: Fix gpg-agent startup

--- shell/env.sh
+++ shell/env.sh
@@ -135,16 +135,16 @@ fi
 #-------------------------------------------------------------
 # GPG
 #-------------------------------------------------------------
-if [[ -f "${HOME}/.gpg-agent-info" ]]; then
-  source "$HOME/.gpg-agent-info"
+if [ -f "${HOME}/.gpg-agent-info" ]; then
+  . "${HOME}/.gpg-agent-info"
   export GPG_AGENT_INFO
   export SSH_AUTH_SOCK
   export SSH_AGENT_PID
 else
-  gpg-agent --daemon \
+  gpg-agent \
+    --daemon \
     --enable-ssh-support \
-    --write-env-file \
-    "${HOME}/.gpg-agent-info"
+    --write-env-file "${HOME}/.gpg-agent-info"
 fi
 
 GPG_TTY=$(tty)
