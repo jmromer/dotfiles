69d6518935ec65fcc2acf7ea8b0374891cd9b4e9
Jake Romer <jkrmr@github.com>
2016-06-10 17:57:21 -0400

    Shell: Disable gpg ssh support

--- shell/env.sh
+++ shell/env.sh
@@ -139,14 +139,11 @@ fi
 if [ ! -f "${HOME}/.gpg-agent-info" ]; then
   gpg-agent \
     --daemon \
-    --enable-ssh-support \
     --write-env-file "${HOME}/.gpg-agent-info" >/dev/null
 fi
 
 source "${HOME}/.gpg-agent-info"
 export GPG_AGENT_INFO
-export SSH_AUTH_SOCK
-export SSH_AGENT_PID
 
 GPG_TTY=$(tty)
 export GPG_TTY
