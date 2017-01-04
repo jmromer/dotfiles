73d575118b415215d678141ddbfde74bbad3c0b7
Jake Romer <jkrmr.io@gmail.com>
2017-01-04 00:26:52 -0500

    Laptop: Kill deprecated --force option

--- laptop/gnu_tools.sh
+++ laptop/gnu_tools.sh
@@ -6,43 +6,43 @@
 brew tap homebrew/dupes
 
 # a collection of binary tools
-brew install --force binutils
+brew install binutils
 
 # core unix tools like ls, etc
-brew install --force coreutils
+brew install coreutils
 
 # diff, cmp, diff3, sdiff
-brew install --force diffutils
+brew install diffutils
 
 # find, locate, updatedb, xargs
-brew install --force findutils --with-default-names
+brew install findutils --with-default-names
 
 # awk: Pattern replacement
-brew install --force gawk
+brew install gawk
 
 # indent: For beautifying C code
-brew install --force gnu-indent --with-default-names
+brew install gnu-indent --with-default-names
 
 # sed: Filtering text
-brew install --force gnu-sed --with-default-names
+brew install gnu-sed --with-default-names
 
 # tar: Creating compressed archives
-brew install --force gnu-tar --with-default-names
+brew install gnu-tar --with-default-names
 
 # which: Find location of given executable
-brew install --force gnu-which --with-default-names
+brew install gnu-which --with-default-names
 
 # TLS: Secure comms
-brew install --force gnutls --with-default-names
+brew install gnutls --with-default-names
 
 # grep: find stuff
-brew install --force grep --with-default-names
+brew install grep --with-default-names
 
 # watch files/dirs for changes
-brew install --force watch
+brew install watch
 
 # word-by-word diffing
-brew install --force wdiff --with-gettext
+brew install wdiff --with-gettext
 
 # easy downloads
-brew install --force wget
+brew install wget
