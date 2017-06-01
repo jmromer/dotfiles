7bf8494f9243cd31003a930005cda535672f4717
Jake Romer <jkrmr.io@gmail.com>
2017-05-31 21:33:53 -0400

    Emacs: Auto-format JS on save

--- home/spacemacs.d/init.el
+++ home/spacemacs.d/init.el
@@ -517,6 +517,11 @@ values."
   ;; Tell js2 mode to shut the hell up about semicolons
   (setq js2-strict-missing-semi-warning nil)
 
+  (eval-after-load 'js2-mode
+    '(add-hook 'js2-mode-hook
+               (lambda ()
+                 (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
   ;; use global ruby rbenv
   (rbenv-use-global)
 
