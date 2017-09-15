1b9c2836c533652763abf85ca9d960eb1b667050
Jake Romer <jkrmr@jakeromer.com>
2017-09-14 23:39:12 -0400

    LaTeX: Compress space between eq and num in org preview

--- home/spacemacs.d/init.el
+++ home/spacemacs.d/init.el
@@ -737,6 +737,28 @@ values."
 The justification is set by :justify in
 `org-format-latex-options'. Only equations at the beginning of a
 line are justified."
+    (setq-default
+     org-format-latex-header
+     "\\documentclass[reqno]{article}
+\\usepackage[usenames]{color}
+[PACKAGES]
+[DEFAULT-PACKAGES]
+\\pagestyle{empty}
+% do not remove
+% The settings below are copied from fullpage.sty
+\\setlength{\\textwidth}{\\paperwidth}
+\\addtolength{\\textwidth}{-10cm}
+\\setlength{\\oddsidemargin}{1.5cm}
+\\addtolength{\\oddsidemargin}{-2.54cm}
+\\setlength{\\evensidemargin}{\\oddsidemargin}
+\\setlength{\\textheight}{\\paperheight}
+\\addtolength{\\textheight}{-\\headheight}
+\\addtolength{\\textheight}{-\\headsep}
+\\addtolength{\\textheight}{-\\footskip}
+\\addtolength{\\textheight}{-3cm}
+\\setlength{\\topmargin}{1.5cm}
+\\addtolength{\\topmargin}{-2.54cm}")
+
     (require 'ov)
     (cond
      ;; Centered justification
