835c759eca2902e6d10d3953e2938f440854752c
Jake Romer <jkrmr@jakeromer.com>
2017-10-19 19:24:40 -0700

    Emacs: Update packages

--- home/spacemacs.d/init.el
+++ home/spacemacs.d/init.el
@@ -966,3 +966,109 @@ Excludes the ibuffer."
   (evil-window-up 1))
 
 ;;; init.el ends here
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(evil-want-Y-yank-to-eol t)
+ '(exec-path-from-shell-arguments (quote ("-l")))
+ '(flycheck-disabled-checkers (quote (javascript-jshint javascript-eslint)))
+ '(highlight-changes-colors (quote ("#FD5FF0" "#AE81FF")))
+ '(highlight-tail-colors
+   (quote
+    (("#20240E" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#20240E" . 100))))
+ '(magit-diff-use-overlays nil)
+ '(org-confirm-babel-evaluate nil)
+ '(org-format-latex-options
+   (quote
+    (:foreground "White" :background default :scale 1.8 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+                 ("begin" "$1" "$" "$$" "\\(" "\\["))))
+ '(package-selected-packages
+   (quote
+    (symon string-inflection sayid ruby-refactor realgud test-simple loc-changes load-relative password-generator org-brain ivy-purpose window-purpose imenu-list impatient-mode godoctor go-rename evil-org evil-lion editorconfig dante cmake-ide levenshtein browse-at-remote ein request-deferred websocket deferred ob-swift auctex-latexmk yapfify yaml-mode xterm-color ws-butler winum which-key wgrep web-mode web-beautify volatile-highlights vmd-mode vimrc-mode vimish-fold vi-tilde-fringe uuidgen use-package unfill toc-org tagedit swift-mode sql-indent spaceline powerline smex smeargle slim-mode shell-pop seeing-is-believing scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe reveal-in-osx-finder restart-emacs request rbenv rainbow-delimiters pyvenv pytest pyenv-mode py-isort pug-mode pretty-mode popwin pip-requirements persp-mode pcre2el pbcopy paradox ox-gfm ov osx-trash osx-dictionary orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-download org-bullets open-junk-file org-plus-contrib ob-restclient ob-http mwim multi-term move-text mmm-mode minitest markdown-toc markdown-mode magit-gitflow magit-gh-pulls macrostep lorem-ipsum livid-mode skewer-mode simple-httpd live-py-mode linum-relative link-hint less-css-mode launchctl json-mode json-snatcher json-reformat js2-refactor js2-mode js-doc ivy-hydra intero info+ indent-guide hyde hy-mode hungry-delete htmlize hlint-refactor hl-todo hindent highlight-parentheses highlight-numbers parent-mode highlight-indentation hide-comnt help-fns+ helm-make haskell-snippets haml-mode graphviz-dot-mode google-translate golden-ratio go-guru go-eldoc gnuplot gitignore-mode github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gist gh marshal logito pcache ht gh-md fuzzy flyspell-correct-ivy flyspell-correct flycheck-pos-tip flycheck-mix flycheck-haskell flycheck-gometalinter flycheck-elm flycheck-credo flycheck flx-ido fill-column-indicator feature-mode eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist projectile-rails rake evil-quickscope evil-numbers evil-matchit evil-magit magit magit-popup git-commit with-editor evil-lisp-state evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-commentary evil-cleverparens smartparens evil-args evil-anzu anzu evil goto-chg undo-tree eshell-z eshell-prompt-extras esh-help engine-mode emmet-mode elm-mode elisp-slime-nav dumb-jump disaster diminish diff-hl deft dash-at-point dactyl-mode cython-mode counsel-projectile projectile counsel-dash helm-dash helm helm-core counsel swiper ivy company-web web-completion-data company-tern dash-functional tern company-statistics company-restclient restclient know-your-http-well company-quickhelp pos-tip company-go go-mode company-ghci company-ghc ghc haskell-mode company-flx flx company-cabal company-c-headers company-auctex company-anaconda command-log-mode column-enforce-mode coffee-mode cmm-mode cmake-mode clojure-snippets clj-refactor hydra inflections edn multiple-cursors paredit peg clean-aindent-mode clang-format cider-eval-sexp-fu eval-sexp-fu highlight cider seq spinner queue clojure-mode chruby bundler inf-ruby bind-map bind-key auto-yasnippet yasnippet auto-highlight-symbol auto-dictionary auto-compile packed auctex async anaconda-mode pythonic f alchemist s company elixir-mode pkg-info epl aggressive-indent adaptive-wrap ace-window ace-link avy ac-ispell auto-complete popup solarized-theme dash)))
+ '(pos-tip-background-color "#A6E22E")
+ '(pos-tip-foreground-color "#272822")
+ '(projectile-git-command "/usr/local/bin/git ls-files -zco --exclude-standard")
+ '(safe-local-variable-values
+   (quote
+    ((projectile-ignored-directories quote
+                                     ("node_modules"))
+     (elixir-enable-compilation-checking . t)
+     (elixir-enable-compilation-checking)
+     (eval setenv "RBENV_VERSION" "system")
+     (eval setq exec-path
+           (append
+            (quote
+             ((concat
+               (projectile-project-root)
+               "bin")))
+            exec-path))
+     (eval setenv "PATH"
+           (concat
+            (projectile-project-root)
+            "bin:"
+            (getenv "PATH")))
+     (eval add-to-list
+           (quote exec-path)
+           (concat
+            (projectile-project-root)
+            "bin")))))
+ '(spacemacs-theme-comment-bg nil t)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (unspecified "#272822" "#20240E" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
+ '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#af0000"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#03A48D"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#718E4B"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#268bd2"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#9e216e"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#4c7173"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#348994"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#a25707")))))
+)
