3429d52a10f5fbece6e51669697dc8ce09d38adf
Jake Romer <mail@jakeromer.com>
2018-12-30 13:51:30 -0500

    Emacs: Customize evil-mode indicators

--- home/spacemacs.d/init.el
+++ home/spacemacs.d/init.el
@@ -287,6 +287,16 @@ values."
   (config/diminish)
   (config/prettify-symbols)
 
+  ;; evil mode indicators
+  (setq-default
+   evil-normal-state-tag   (propertize "[Normal]" 'face '((:background "green" :foreground "black")))
+   evil-emacs-state-tag    (propertize "[Emacs]" 'face '((:background "orange" :foreground "black")))
+   evil-insert-state-tag   (propertize "[Insert]" 'face '((:background "red") :foreground "white"))
+   evil-hybrid-state-tag   (propertize "[Hybrid]" 'face '((:background "red") :foreground "white"))
+   evil-motion-state-tag   (propertize "[Motion]" 'face '((:background "blue") :foreground "white"))
+   evil-visual-state-tag   (propertize "[Visual]" 'face '((:background "grey80" :foreground "black")))
+   evil-operator-state-tag (propertize "[Operator]" 'face '((:background "purple"))))
+
   (require 'doom-modeline)
   (doom-modeline-init)
 
