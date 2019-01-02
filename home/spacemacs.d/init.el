ea8763ac5de363b7a4fe2ced8f651f97fae0b3ee
Jake Romer <mail@jakeromer.com>
2019-01-01 20:32:51 -0500

    Emacs: Bump which-key delay

--- home/spacemacs.d/init.el
+++ home/spacemacs.d/init.el
@@ -339,7 +339,7 @@ values."
 
    ;; Which-key delay in seconds. The which-key buffer is the popup listing
    ;; the commands bound to the current keystroke sequence. (default 0.4)
-   dotspacemacs-which-key-delay 0.2
+   dotspacemacs-which-key-delay 0.3
 
    ;; Which-key frame position. Possible values are `right', `bottom' and
    ;; `right-then-bottom'. right-then-bottom tries to display the frame to the
