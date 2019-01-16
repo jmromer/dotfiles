afbb1a42d854f99b41a6b9e2c1bd7d752916d953
Jake Romer <mail@jakeromer.com>
2019-01-16 13:58:45 -0500

    Ruby: Update rails_template

--- rails_template.rb
+++ rails_template.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 gem_group :development, :test do
-  gem "factory_girl_rails"
+  gem "factory_bot_rails"
   gem "jazz_fingers"
   gem "pry-byebug"
   gem "pry-rails"
@@ -12,9 +12,8 @@ end
 
 gem_group :test do
   gem "capybara"
-  # gem "shoulda-matchers",
-  #    github: "thoughtbot/shoulda-matchers",
-  #    branch: "rails-5"
+  gem "rails-controller-testing"
+  gem "shoulda-matchers"
 end
 
 gem_group :development do
@@ -26,12 +25,12 @@ run "echo \"* $(basename $(pwd))\" >> README.org"
 
 ## add config initializer
 initializer "generators.rb", <<~RUBY
-  Rails.application.config.generators do |g|
-    g.factory_girl false
-    g.helper false
-    g.javascripts false
-    g.stylesheets false
-    g.test_framework :rspec, view_specs: false, routing_specs: false
-    g.template_engine nil
-  end
-RUBY
+              Rails.application.config.generators do |g|
+                g.factory_bot false
+                g.helper false
+                g.javascripts false
+                g.stylesheets false
+                g.test_framework :rspec, view_specs: false, routing_specs: false
+                g.template_engine nil
+              end
+            RUBY
