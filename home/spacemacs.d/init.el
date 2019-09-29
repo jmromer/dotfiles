55f3d0ee1cc77567b4446b75d9d2440e8d701ad5
Jake Romer <mail@jakeromer.com>
2019-09-29 08:41:04 -0400

    Emacs: projectile-git-submodule-command workaround

--- home/spacemacs.d/init.el
+++ home/spacemacs.d/init.el
@@ -1802,6 +1802,7 @@ Only equations at the beginning of a line are justified."
   (setq-default projectile-completion-system 'ivy
                 projectile-enable-caching t
                 projectile-find-dir-includes-top-level t
+                projectile-git-submodule-command nil
                 projectile-project-search-path '("~/Projects" "~/Work"))
 
   (if (bound-and-true-p projectile-globally-ignored-directories)
