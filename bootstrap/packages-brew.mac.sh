574a3a8babc588b1dc479499a09fa948076f5bf2
Jake Romer <mail@jakeromer.com>
2020-04-11 22:16:53 -0400

    Dotfiles: Install emacs-mac instead of emacs-plus

--- bootstrap/packages-brew.mac.sh
+++ bootstrap/packages-brew.mac.sh
@@ -10,13 +10,22 @@ brew install pinentry-mac
 brew install reattach-to-user-namespace
 brew install github/gh/gh
 
-# brew tap railwaycat/emacsmacport
-# brew cask install emacs-mac-spacemacs-icon
+brew tap railwaycat/emacsmacport
+brew install emacs-mac \
+  --with-dbus \
+  --with-glib \
+  --with-imagemagick \
+  --with-modules \
+  --with-natural-title-bar \
+  --with-rsvg \
+  --with-spacemacs-icon \
+  --with-xml2
+ln -sf /usr/local/opt/emacs-mac/Emacs.app /Applications
 
-brew tap d12frosted/emacs-plus
-brew install emacs-plus --with-no-frame-refocus --with-no-titlebar
-ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
-brew services start d12frosted/emacs-plus/emacs-plus
+# brew tap d12frosted/emacs-plus
+# brew install emacs-plus --with-no-frame-refocus --with-no-titlebar
+# ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
+# brew services start d12frosted/emacs-plus/emacs-plus
 
 emacs_version="$(emacs --version | head -1 | sed -E 's/.+\s([0-9]+\.[0-9]+)$/\1/')"
 tic -o ~/.terminfo "/usr/local/share/emacs/$emacs_version/etc/e/eterm-color.ti"
