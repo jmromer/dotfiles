6b7d235dfa79cbe03b58612d0cfcd3ecfb5a1f6e
Jake Romer <mail@jakeromer.com>
2020-04-16 07:52:40 -0400

    Emacs: Add org mode super-key keybindings

--- home/spacemacs.d/customizations.el
+++ home/spacemacs.d/customizations.el
@@ -145,7 +145,8 @@
         (define-key org-mode-map (kbd "s-j") #'org-occur)
         (define-key org-mode-map (kbd "s-F") #'avy-org-goto-heading-timer)
         (define-key org-mode-map (kbd "s-r") #'avy-org-refile-as-child)
-        (define-key org-mode-map (kbd "s-<return>") #'evil-org-org-insert-heading-respect-content-below))
+        (define-key org-mode-map (kbd "s-<return>") #'org-insert-subheading)
+        (define-key org-mode-map (kbd "s-S-<return>") #'evil-org-org-insert-heading-respect-content-below))
     (error "Failed setting org mode super-key keybindings")))
 
 ;; cycle theme
