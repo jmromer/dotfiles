21544ffa5960fba4d4d147535e1521e7d8bfd646
Jake Romer <mail@jakeromer.com>
2020-04-22 14:44:25 -0400

    Emacs: Clean up setup.el

--- home/spacemacs.d/setup.el
+++ home/spacemacs.d/setup.el
@@ -6,6 +6,9 @@
 (require 'funcs)
 (require 'editorconfig)
 
+
+;;; Package configs
+
 (config/copy-as-format)
 (config/evil-collection)
 (config/evil-goggles)
@@ -25,11 +28,15 @@
 (config/underscore-to-word-char-list)
 (config/web-mode)
 
-;; misc settings
+
+;;; Misc settings
+
 (setq-default
  persp-restore-window-conf-method #'no-op
  browse-url-browser-function 'xwidget-webkit-browse-url
  compilation-scroll-output 'first-error
+ ido-enable-flex-matching t
+ ido-use-faces nil  ;; disable ido faces to see flx highlights
  ispell-program-name "ispell"
  lsp-signature-auto-activate nil  ;; lsp: don't auto-activate signature hints
  json-fmt-on-save nil
@@ -66,67 +73,88 @@
       (add-to-list 'projectile-globally-ignored-directories "node_modules" t)))
 
 
-;; Appearance: Enable transparency
+
+;;; Appearance
+
 ;; ensure new frames are created transparent
 (add-hook 'after-make-frame-functions #'spacemacs/enable-transparency)
+
 ;; make the current one transparent
 (spacemacs/enable-transparency)
 
-;; Appearance: Natural titlebar
+;; Natural titlebar in GUI
 (when window-system
   (add-to-list 'default-frame-alist '(ns-appearance . dark))
   (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t)))
 
-;; Appearance: Render emoji
 
+
+;;; Emoji
+
+;; Render unicode emoji as emoji
 (defun set-emoji-font (frame)
-  "Adjust the font settings of FRAME so Emacs can render emoji codes as emoji."
+  "Render unicode emoji as emoji.
+Adjust the font settings of the given FRAME to do this."
   (if (eq system-type 'darwin)
       (set-fontset-font t 'unicode (font-spec :family "Apple Color Emoji") frame 'prepend)
     (set-fontset-font t 'unicode (font-spec :family "Symbola") frame 'prepend)))
 
-(if window-system (set-emoji-font nil)
+(if window-system
+    (set-emoji-font nil)
   (add-hook 'after-make-frame-functions #'set-emoji-font))
 
-;; globally enabled minor modes
+
+
+;;; Globally enabled minor modes
+
 (beacon-mode)
 (editorconfig-mode)
 (global-evil-matchit-mode)
 (global-evil-quickscope-mode)
+(ido-mode)  ;; ido (for amx, ido-dired)
+(flx-ido-mode 1)
 (direnv-mode)
 (smartparens-global-strict-mode)
 (global-visual-line-mode)
 (company-prescient-mode)
 (prescient-persist-mode)
 
-;; ido (for amx, ido-dired)
-(ido-mode)
-(flx-ido-mode 1)
-(setq-default
- ido-enable-flex-matching t
- ;; disable ido faces to see flx highlights
- ido-use-faces nil)
 
-;; garbage collection optimizations
-(defun disable-garbage-collection ()
-  "Disable GC by setting threshold to max num."
-  (setq-default gc-cons-threshold most-positive-fixnum))
-(defun enable-garbage-collection ()
-  "Re-enable GC collection. Set threshold to init.el value."
-  (setq-default gc-cons-threshold (car dotspacemacs-gc-cons)))
-(add-hook 'minibuffer-setup-hook #'disable-garbage-collection)
-(add-hook 'minibuffer-exit-hook #'enable-garbage-collection)
-
-;; misc hooks
-(add-hook 'elixir-mode-hook #'lsp)
-(add-hook 'ledger-mode-hook #'evil-ledger-mode)
+
+;; Hooks
 
 ;; after-init hooks
 (add-hook 'after-init-hook #'global-emojify-mode)
 (add-hook 'after-init-hook #'global-company-mode)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-;; Post-config
+;; mode-specific hooks
+(add-hook 'elixir-mode-hook #'lsp)
+(add-hook 'ledger-mode-hook #'evil-ledger-mode)
+
+(with-eval-after-load 'elm-mode
+  (remove-hook 'elm-mode-hook 'elm-indent-mode))
+
+
+
+;;; Garbage collection optimizations
+
+(defun disable-garbage-collection ()
+  "Disable GC by setting threshold to max num."
+  (setq-default gc-cons-threshold most-positive-fixnum))
+
+(defun enable-garbage-collection ()
+  "Re-enable GC collection. Set threshold to init.el value."
+  (defvar dotspacemacs-gc-cons)
+  (setq-default gc-cons-threshold (car dotspacemacs-gc-cons)))
+
+(add-hook 'minibuffer-setup-hook #'disable-garbage-collection)
+(add-hook 'minibuffer-exit-hook #'enable-garbage-collection)
+
+
+
+;;; "Tail" settings (should be run last)
+
 (config/prettify-symbols)
 (config/company)
 
