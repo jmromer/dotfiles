32c6f551ef8cf1ab348da0103e65b9fc1bb3af94
Jake Romer <mail@jakeromer.com>
2021-01-05 20:30:42 -0500

    Bootstrap: Update mac packages manifest

--- bootstrap/packages-brew.mac.sh
+++ bootstrap/packages-brew.mac.sh
@@ -12,35 +12,22 @@ brew install github/gh/gh
 
 # emacs-mac (potentially slower / unstable)
 # brew tap railwaycat/emacsmacport
-# brew install emacs-mac \
-#   --with-dbus \
-#   --with-glib \
-#   --with-imagemagick \
-#   --with-modern-icon \
-#   --with-modules \
-#   --with-natural-title-bar \
-#   --with-rsvg \
-#   --with-xml2
-# ln -sf /usr/local/opt/emacs-mac/Emacs.app /Applications
+# brew install emacs-mac
 
 # emacs-plus
 # brew tap d12frosted/emacs-plus
 # brew install emacs-plus
-# brew services start d12frosted/emacs-plus/emacs-plus
-# ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
-
 # emacs_version="$(emacs --version | head -1 | sed -E 's/.+\s([0-9]+\.[0-9]+)$/\1/')"
 # tic -o ~/.terminfo "/usr/local/share/emacs/$emacs_version/etc/e/eterm-color.ti"
 
 brew tap daviderestivo/emacs-head
-brew install emacs-head \
-  --HEAD \
+brew install emacs-head@28 \
   --with-cocoa \
   --with-imagemagick \
-  --with-jansson \
   --with-modern-icon-black-variant \
   --with-no-frame-refocus \
   --with-pdumper \
-  --with-xwidgets \
-ln -s /usr/local/opt/emacs-head/Emacs.app /Applications
-tic -o ~/.terminfo /usr/local/opt/emacs-head/share/emacs/28.0.50/etc/e/eterm-color.ti
+  --with-xwidgets
+
+ln -sf /usr/local/opt/emacs-head/Emacs.app /Applications
+tic -o ~/.terminfo /usr/local/opt/emacs-head@28/share/emacs/28.0.50/etc/e/eterm-color.ti
