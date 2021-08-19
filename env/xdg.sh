9072447a00995e779ab46a29e786a7ef01db04b6
Jake Romer <jake@jmromer.org>
2021-08-19 15:20:26 -0400

    Note which XDG-related vars are set in LaunchAgents

--- env/xdg.sh
+++ env/xdg.sh
@@ -3,7 +3,7 @@ export DOTFILES_DIR="${HOME}/.dotfiles"
 
 # user-specific data locations
 export XDG_DATA_HOME="${DOTFILES_DIR}/share"     # data files
-export XDG_CONFIG_HOME="${DOTFILES_DIR}/config"  # configuration files
+export XDG_CONFIG_HOME="${DOTFILES_DIR}/config"  # configuration files (also set in LaunchAgent)
 export XDG_STATE_HOME="${DOTFILES_DIR}/state"    # state files (logs, history, recently used files, current app state, etc)
 export XDG_CACHE_HOME="${DOTFILES_DIR}/cache"    # ephemeral data files
 export XDG_RUNTIME_DIR="${DOTFILES_DIR}/sockets" # ephemeral runtime files (sockets, named pipes, etc)
@@ -14,5 +14,5 @@ export XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
 # base directories to search for configuration files in addition to the $XDG_CONFIG_HOME base directory.
 export XDG_CONFIG_DIRS="/etc/xdg"
 
-# zsh config and ephemeral files locations
+# zsh config and ephemeral files locations (also set in LaunchAgent)
 export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
