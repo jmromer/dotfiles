3e8700008d3d5a2c5a7bf07061034aa8f054d3bb
Jake Romer <jake@jmromer.org>
2021-08-19 10:14:54 -0400

    Add xdg environment variable launch agent

---
+++ env/xdg.sh
@@ -0,0 +1,18 @@
+# dotfiles dir (configs and permanent)
+export DOTFILES_DIR="${HOME}/.dotfiles"
+
+# user-specific data locations
+export XDG_DATA_HOME="${DOTFILES_DIR}/share"     # data files
+export XDG_CONFIG_HOME="${DOTFILES_DIR}/config"  # configuration files
+export XDG_STATE_HOME="${DOTFILES_DIR}/state"    # state files (logs, history, recently used files, current app state, etc)
+export XDG_CACHE_HOME="${DOTFILES_DIR}/cache"    # ephemeral data files
+export XDG_RUNTIME_DIR="${DOTFILES_DIR}/sockets" # ephemeral runtime files (sockets, named pipes, etc)
+
+# base directories to search for data files in addition to the $XDG_DATA_HOME base directory.
+export XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
+
+# base directories to search for configuration files in addition to the $XDG_CONFIG_HOME base directory.
+export XDG_CONFIG_DIRS="/etc/xdg"
+
+# zsh config and ephemeral files locations
+export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
