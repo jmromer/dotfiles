1b75d43e6e848920d892a1b65c13bf5c049aeb68
Jake Romer <jake@jmromer.org>
2021-08-20 20:20:47 -0400

    Set FZF default commands

--- env/fzf.sh
+++ env/fzf.sh
@@ -5,7 +5,9 @@ if [[ -z "${XDG_DATA_HOME}" ]]; then
   echo "WARNING: XDG_DATA_HOME env var is not set in $0"
 fi
 
-# FZF_DEFAULT_COMMAND
-# FZF_CTRL_T_COMMAND
 export FZF_DIR="${XDG_DATA_HOME}/fzf"
 export FZF_DEFAULT_OPTS="--no-multi --tiebreak=index --bind='ctrl-f:preview-down' --bind='ctrl-b:preview-up'"
+
+# export FZF_CTRL_T_COMMAND="command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
+export FZF_DEFAULT_COMMAND="fd --hidden --type f --exclude .git --exclude node_modules"
+export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
