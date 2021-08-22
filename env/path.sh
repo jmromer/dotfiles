1e14bbad90b74940520b4b8a6a8e9a076d1ee084
Jake Romer <jake@jmromer.org>
2021-08-22 13:57:33 -0400

    Add workaround for GNU/BSD uname discrepancy

--- env/path.sh
+++ env/path.sh
@@ -20,6 +20,8 @@ PATH+=":${XDG_DATA_HOME}/emacs/bin"
 PATH+=":${XDG_DATA_HOME}/npm/bin"
 PATH+=":${XDG_DATA_HOME}/gem/ruby/3.0.0/bin"
 
+PATH+=":${HOMEBREW_PREFIX}/bin"
+PATH+=":${HOMEBREW_PREFIX}/sbin"
 PATH+=":${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/curl/bin"
 PATH+=":${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin"
@@ -34,8 +36,6 @@ PATH+=":${HOMEBREW_PREFIX}/opt/make/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/rg/bin"
 PATH+=":${HOMEBREW_PREFIX}/share/git-core/contrib/diff-highlight"
 
-PATH+=":${HOMEBREW_PREFIX}/bin"
-PATH+=":${HOMEBREW_PREFIX}/sbin"
 PATH+=":/usr/bin"
 PATH+=":/bin"
 PATH+=":/usr/sbin"
@@ -43,3 +43,7 @@ PATH+=":/sbin"
 
 PATH+=":/Library/TeX/texbin"
 export PATH
+
+# Prefer BSD uname, becuase it reports m1 arch as 'arm'
+# GNU uname reports it as 'arm64', which breaks some build scripts
+ln -sf /usr/bin/uname "${HOMEBREW_PREFIX}/bin/uname"
