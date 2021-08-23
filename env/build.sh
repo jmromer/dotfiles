82562805cef90aa308faabcd1042f70a1cb4fd16
Jake Romer <jake@jmromer.org>
2021-08-23 10:47:48 -0400

    Reorganize env/build.sh
    Provide shell functions to set, re-set, clear build flags.
    This can tighten the feedback look when bisection-debugging compiler
    compatibility issues.

--- env/build.sh
+++ env/build.sh
@@ -5,50 +5,160 @@ fi
 #-------------------------------------------------------------
 # Compilation flags
 #-------------------------------------------------------------
-export CC=gcc-11
-export CXX=g++-11
+BUILD_VARS='CC CXX CFLAGS CPPFLAGS LDFLAGS PKG_CONFIG_PATH RUBY_CONFIGURE_OPTS RUBY_GC_MALLOC_LIMIT RUBY_GC_HEAP_FREE_SLOTS WARNFLAGS'
 
-LDFLAGS="  -L${HOMEBREW_PREFIX}/opt/gettext/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/icu4c/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/libffi/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/libxml2/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/llvm/lib -Wl,-rpath,${HOMEBREW_PREFIX}/opt/llvm/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/ncurses/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/openssl@1.1/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/readline/lib"
-LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/zlib/lib"
-export LDFLAGS
+build_flags_unset() {
+  for var in $(echo "$BUILD_VARS"); do
+    unset "$var"
+  done
+}
 
-CPPFLAGS="  -I${HOMEBREW_PREFIX}/opt/gettext/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/icu4c/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libffi/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libxml2/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/llvm/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/ncurses/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/openssl@1.1/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/readline/include"
-CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/zlib/include"
-export CPPFLAGS
+build_flags_export() {
+  for var in $(echo "$BUILD_VARS"); do
+    export "$var"
+  done
+}
 
-PKG_CONFIG_PATH="${HOMEBREW_PREFIX}/opt/icu4c/lib/pkgconfig"
-PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/imagemagick/lib/pkgconfig"
-PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/libffi/lib/pkgconfig"
-PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/libxml2/lib/pkgconfig"
-PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/ncurses/lib/pkgconfig"
-PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/openssl@1.1/lib/pkgconfig"
-PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/readline/lib/pkgconfig"
-PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/zlib/lib/pkgconfig"
-export PKG_CONFIG_PATH
+build_flags_inspect() {
+  echo """
+  CC: ${CC}
 
+  CXX: ${CXX}
 
-RUBY_CONFIGURE_OPTS="  --with-readline-dir=${HOMEBREW_PREFIX}/opt/readline"
-RUBY_CONFIGURE_OPTS+=" --with-openssl-dir=${HOMEBREW_PREFIX}/opt/openssl@1.1"
-RUBY_CONFIGURE_OPTS+=" --enable-shared"
-RUBY_CONFIGURE_OPTS+=" --disable-libedit"
-export RUBY_CONFIGURE_OPTS
+  WARNFLAGS: ${WARNFLAGS// -/\n-}
 
-export CFLAGS="-O3 -g -I${HOMEBREW_PREFIX}/opt/openssl@1.1/include"
-export OPTFLAGS="-Wno-error=implicit-function-declaration"
-export RUBY_CFLAGS="-march=native -Os ${OPTFLAGS}"
-export RUBY_GC_MALLOC_LIMIT=60000000
-export RUBY_GC_HEAP_FREE_SLOTS=200000
+  CFLAGS: ${CFLAGS// -I/\n-I}
+
+  CPPFLAGS: ${CPPFLAGS// -I/\n-I}
+
+  LDFLAGS: ${LDFLAGS// -L/\n-L}
+
+  PKG_CONFIG_PATH: ${PKG_CONFIG_PATH//:/\n}
+
+  RUBY_CONFIGURE_OPTS: ${RUBY_CONFIGURE_OPTS// -/\n-}
+  RUBY_GC_MALLOC_LIMIT: ${RUBY_GC_MALLOC_LIMIT}
+  RUBY_GC_HEAP_FREE_SLOTS: ${RUBY_GC_HEAP_FREE_SLOTS}
+  """ | sed -E 's/^ +//g' | less
+}
+
+build_flags_reset() {
+  build_flags_unset
+  build_flags_set "$@"
+}
+
+build_flags_set() {
+  local quiet_mode
+
+  while [ $# -gt -1 ]; do
+    case "$1" in
+      gettext)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/gettext/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/gettext/include"
+        shift
+        ;;
+      icu4c)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/icu4c/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/icu4c/include"
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/icu4c/lib/pkgconfig"
+        shift
+        ;;
+      libffi)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/libffi/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libffi/include"
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/libffi/lib/pkgconfig"
+        shift
+        ;;
+      libxml2)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/libxml2/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libxml2/include"
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/libxml2/lib/pkgconfig"
+        shift
+        ;;
+      ncurses)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/ncurses/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/ncurses/include"
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/ncurses/lib/pkgconfig"
+        shift
+        ;;
+      llvm)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/llvm/lib -Wl,-rpath,${HOMEBREW_PREFIX}/opt/llvm/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/llvm/include"
+        shift
+        ;;
+      openssl)
+        OPENSSL="openssl@1.1"
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/openssl@1.1/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/openssl@1.1/include"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/openssl@1.1/include"
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/openssl@1.1/lib/pkgconfig"
+        RUBY_CONFIGURE_OPTS+=" --with-openssl-dir=${HOMEBREW_PREFIX}/opt/openssl@1.1"
+        shift
+        ;;
+      readline)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/readline/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/readline/include"
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/readline/lib/pkgconfig"
+        RUBY_CONFIGURE_OPTS+=" --with-readline-dir=${HOMEBREW_PREFIX}/opt/readline"
+        shift
+        ;;
+      zlib)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/zlib/lib"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/zlib/include"
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/zlib/lib/pkgconfig"
+        shift
+        ;;
+      imagemagick)
+        PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/imagemagick/lib/pkgconfig"
+        shift
+        ;;
+      ruby)
+        RUBY_CONFIGURE_OPTS+=" --enable-shared"
+        RUBY_CONFIGURE_OPTS+=" --disable-libedit"
+        RUBY_GC_MALLOC_LIMIT=60000000
+        RUBY_GC_HEAP_FREE_SLOTS=200000
+        WARNFLAGS="-Wno-error=implicit-function-declaration"
+        shift
+        ;;
+      c)
+        CFLAGS+=" -O3 -g"
+        shift
+        ;;
+      clang++|g++*)
+        CXX="$1"
+        shift
+        ;;
+      clang|gcc*)
+        CC="$1"
+        shift
+        ;;
+      --quietly|-q)
+        quiet_mode=1
+        shift
+        ;;
+      "")
+        build_flags_export
+        [ ! $quiet_mode ] && build_flags_inspect
+        return
+        ;;
+      *)
+        echo "Unrecognized: $1"
+        shift
+        ;;
+    esac
+  done
+}
+
+build_flags_set \
+  --quietly \
+  gettext \
+  icu4c \
+  imagemagick \
+  libffi \
+  libxml2 \
+  llvm \
+  ncurses \
+  openssl \
+  readline \
+  ruby \
+  zlib \
+  gcc-11
