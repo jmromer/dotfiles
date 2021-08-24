0bae141c115d3921671ea713ae7a0653463be756
Jake Romer <jake@jmromer.org>
2021-08-23 21:45:44 -0400

    Add some env script dependency warnings

--- env/bundler.sh
+++ env/bundler.sh
@@ -1,3 +1,7 @@
+if [[ -z "${MACHINE_CORES}" ]]; then
+  echo "WARNING: MACHINE_CORES env var is not set in ${0}"
+fi
+
 export BUNDLE_JOBS="${MACHINE_CORES}"
 export BUNDLE_CONSOLE="pry"
 export BUNDLE_GEM__COC="false"
