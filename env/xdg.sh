fd7b23359bac549b81ef92030ff0a6aa7fcba11d
Jake Romer <jake@jmromer.org>
2021-08-26 12:39:36 -0400

    Add rediscli xdg history file location

--- env/xdg.sh
+++ env/xdg.sh
@@ -9,8 +9,8 @@ export XDG_STATE_HOME="${DOTFILES_DIR}/state"    # state files (logs, history, r
 export XDG_CACHE_HOME="${DOTFILES_DIR}/cache"    # ephemeral data files
 
 export XDG_RUNTIME_DIR="${DOTFILES_DIR}/sockets" # ephemeral runtime files (sockets, named pipes, etc)
-export XDG_LOCALS_DIR="${DOTFILES_DIR}/locals"   # un-synced, un-tracked dotfiles
-export XDG_SECURE_DIR="${DOTFILES_DIR}/secure"   # synced, tracked sensitive dotfiles
+export XDG_LOCALS_DIR="${DOTFILES_DIR}/locals"   # un-synced, un-tracked dotfiles (not spec)
+export XDG_SECURE_DIR="${DOTFILES_DIR}/secure"   # synced, tracked sensitive dotfiles (not spec)
 
 # base directories to search for data files in addition to the $XDG_DATA_HOME base directory.
 export XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
@@ -18,32 +18,18 @@ export XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
 # base directories to search for configuration files in addition to the $XDG_CONFIG_HOME base directory.
 export XDG_CONFIG_DIRS="/etc/xdg"
 
-# zsh config and ephemeral files locations (also set in LaunchAgent)
-export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
-
-# vim config (ephemeral files config therein)
-export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc"
-
-# doom
+export BUNDLE_CACHE_PATH="${XDG_CACHE_HOME}/bundler"
 export DOOMDIR="${XDG_CONFIG_HOME}/doom"
-
-# gnupg
+export GEMRC="${XDG_CONFIG_HOME}/gem/gemrc"
 export GNUPGHOME="${XDG_SECURE_DIR}/gnupg"
-
-# less
+export INPUTRC="${XDG_CONFIG_HOME}/inputrc"
+export IPYTHONDIR="${XDG_DATA_HOME}/ipython"
+export JUPYTER_CONFIG_DIR="${XDG_CONFIG_HOME}/jupyter"
 export LESSHISTFILE="${XDG_STATE_HOME}/less/history"
 export LESSKEY="${XDG_CONFIG_HOME}/less/keys"
-
-# misc
-export INPUTRC="${XDG_CONFIG_HOME}/inputrc"
-export JUPYTER_CONFIG_DIR="${XDG_CONFIG_HOME}/jupyter"
-export IPYTHONDIR="${XDG_DATA_HOME}/ipython"
-
-# npm
-export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 export NPM_CONFIG_CACHE="${XDG_CACHE_HOME}/npm"
 export NPM_CONFIG_PREFIX="${XDG_DATA_HOME}/npm"
-
-# bundler, rubygems
-export BUNDLE_CACHE_PATH="${XDG_CACHE_HOME}/bundler"
-export GEMRC="${XDG_CONFIG_HOME}/gem/gemrc"
+export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
+export REDISCLI_HISTFILE="${XDG_STATE_HOME}/redis/rediscli_history"
+export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc"
+export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
