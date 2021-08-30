0deaeeee6c1693067c3a6884f990ffd661e717eb
Jake Romer <jake@jmromer.org>
2021-08-30 13:59:09 -0400

    Update build environment script values

--- env/build.sh
+++ env/build.sh
@@ -5,7 +5,7 @@ fi
 #-------------------------------------------------------------
 # Compilation flags
 #-------------------------------------------------------------
-BUILD_VARS='CC CXX CFLAGS CPPFLAGS LDFLAGS PKG_CONFIG_PATH RUBY_CONFIGURE_OPTS RUBY_GC_MALLOC_LIMIT RUBY_GC_HEAP_FREE_SLOTS WARNFLAGS'
+BUILD_VARS='CC CXX CFLAGS CPPFLAGS LDFLAGS PKG_CONFIG_PATH RUBY_CONFIGURE_OPTS RUBY_GC_MALLOC_LIMIT RUBY_GC_HEAP_FREE_SLOTS WARNFLAGS OPTFLAGS'
 
 build_flags_unset() {
   for var in $(echo "$BUILD_VARS"); do
@@ -27,6 +27,8 @@ build_flags_inspect() {
 
   WARNFLAGS: ${WARNFLAGS// -/\n-}
 
+  OPTFLAGS: ${OPTFLAGS// -/\n-}
+
   CFLAGS: ${CFLAGS// -I/\n-I}
 
   CPPFLAGS: ${CPPFLAGS// -I/\n-I}
@@ -36,7 +38,9 @@ build_flags_inspect() {
   PKG_CONFIG_PATH: ${PKG_CONFIG_PATH//:/\n}
 
   RUBY_CONFIGURE_OPTS: ${RUBY_CONFIGURE_OPTS// -/\n-}
+
   RUBY_GC_MALLOC_LIMIT: ${RUBY_GC_MALLOC_LIMIT}
+
   RUBY_GC_HEAP_FREE_SLOTS: ${RUBY_GC_HEAP_FREE_SLOTS}
   """ | sed -E 's/^ +//g' | less
 }
@@ -51,37 +55,49 @@ build_flags_set() {
 
   while [ $# -gt -1 ]; do
     case "$1" in
+      bzip2)
+        LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/bzip2/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/bzip2/include"
+        CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/bzip2/include"
+        shift
+        ;;
       gettext)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/gettext/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/gettext/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/gettext/include"
         shift
         ;;
       icu4c)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/icu4c/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/icu4c/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/icu4c/include"
         PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/icu4c/lib/pkgconfig"
         shift
         ;;
       libffi)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/libffi/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libffi/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libffi/include"
         PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/libffi/lib/pkgconfig"
         shift
         ;;
       libxml2)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/libxml2/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libxml2/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/libxml2/include"
         PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/libxml2/lib/pkgconfig"
         shift
         ;;
       ncurses)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/ncurses/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/ncurses/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/ncurses/include"
         PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/ncurses/lib/pkgconfig"
         shift
         ;;
       llvm)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/llvm/lib -Wl,-rpath,${HOMEBREW_PREFIX}/opt/llvm/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/llvm/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/llvm/include"
         shift
         ;;
@@ -96,13 +112,22 @@ build_flags_set() {
         ;;
       readline)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/readline/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/readline/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/readline/include"
         PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/readline/lib/pkgconfig"
         RUBY_CONFIGURE_OPTS+=" --with-readline-dir=${HOMEBREW_PREFIX}/opt/readline"
         shift
         ;;
+      xcrun)
+        sdk_path="$(xcrun --show-sdk-path)"
+        LDFLAGS+=" -L${sdk_path}/usr/lib"
+        CFLAGS+=" -I${sdk_path}/usr/include"
+        CPPFLAGS+=" -I${sdk_path}/usr/include"
+        shift
+        ;;
       zlib)
         LDFLAGS+=" -L${HOMEBREW_PREFIX}/opt/zlib/lib"
+        CFLAGS+=" -I${HOMEBREW_PREFIX}/opt/zlib/include"
         CPPFLAGS+=" -I${HOMEBREW_PREFIX}/opt/zlib/include"
         PKG_CONFIG_PATH+=":${HOMEBREW_PREFIX}/opt/zlib/lib/pkgconfig"
         shift
@@ -116,11 +141,6 @@ build_flags_set() {
         RUBY_CONFIGURE_OPTS+=" --disable-libedit"
         RUBY_GC_MALLOC_LIMIT=60000000
         RUBY_GC_HEAP_FREE_SLOTS=200000
-        WARNFLAGS="-Wno-error=implicit-function-declaration"
-        shift
-        ;;
-      c)
-        CFLAGS+=" -O3 -g"
         shift
         ;;
       clang++|g++*)
@@ -129,6 +149,24 @@ build_flags_set() {
         ;;
       clang|gcc*)
         CC="$1"
+        shift
+        ;;
+      no-warnings)
+        unset WARNFLAGS
+        WARNFLAGS+=" -w"         # Inhibit warnings
+        WARNFLAGS+=" -Wno-error" # Don't turn warnings into errors (override)
+        shift
+        ;;
+      optimize*)
+        unset OPTFLAGS
+        level="${1/optimize/}"
+        if [[ -n "${level}" ]]; then
+          # Optimization level 3 (max)
+          # (NB: Can cause some builds to fail. Un-setting or lower can help.)
+          OPTFLAGS+=" -O${level}"
+        fi
+
+        OPTFLAGS+=" -g" # Produce debugging information
         shift
         ;;
       --quietly|-q)
@@ -150,6 +188,9 @@ build_flags_set() {
 
 build_flags_set \
   --quietly \
+  bzip2 \
+  g++-11 \
+  gcc-11 \
   gettext \
   icu4c \
   imagemagick \
@@ -157,9 +198,10 @@ build_flags_set \
   libxml2 \
   llvm \
   ncurses \
+  no-warnings \
   openssl \
+  optimize3 \
   readline \
   ruby \
-  zlib \
-  gcc-11 \
-  g++-11
+  xcrun \
+  zlib
