49c4013be781e718d303c4878e419a63245ec33b
Jake Romer <jake@jmromer.org>
2021-09-02 21:18:59 -0400

    Add markdown grip, org-noter, ox-hugo, org-journal

--- config/doom/init.el
+++ config/doom/init.el
@@ -137,18 +137,18 @@
        ;; idris              ; a language you can depend on
        json                  ; At least it ain't XML
        ;; (java +meghanada)  ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)            ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)     ; all(hope(abandon(ye(who(enter(here))))))
        ;; julia              ; a better, faster MATLAB
        ;; kotlin             ; a better, slicker Java(Script)
        ;; latex              ; writing papers in Emacs has never been so fun
        ;; lean               ; for folks with too much to prove
        ;; ledger             ; be audit you can be
        ;; lua                ; one-based indices? one-based indices
-       markdown              ; writing docs for people to ignore
+       (markdown +grip)      ; writing docs for people to ignore
        ;; nim                ; python + lisp at the speed of c
        ;; nix                ; I hereby declare "nix geht mehr!"
        ;; ocaml              ; an objective camel
-       org                   ; organize your plain life in plain text
+       (org +noter +hugo +journal)  ; organize your plain life in plain text
        ;; php                ; perl's insecure younger brother
        ;; plantuml           ; diagrams for confusing people more
        ;; purescript         ; javascript, but functional
