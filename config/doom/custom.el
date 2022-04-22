47cf8f2b6ce72e890bcca4225100c353e9802d38
Jake Romer <jake@jmromer.org>
2022-04-22 14:29:16 -0400

    Update emacs custom.el

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((lsp-yaml-format-enable)
+     (lsp-typescript-format-enable)
+     (lsp-javascript-format-enable)))
  '(warning-suppress-types '((lsp-mode) (lsp-mode) (lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
