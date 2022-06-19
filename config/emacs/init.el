7d9245bcda1d61ef41795dd141c490fd17e69590
Jake Romer <jake@jmromer.org>
2022-06-18 23:34:48 -0400

    Update for emacs init change

--- config/emacs/init.el
+++
@@ -1,9 +0,0 @@
-;;; init.el -*- lexical-binding: t; -*-
-
-(setq user-emacs-directory
-      (format "%s/%s"
-              (or (getenv "XDG_DATA_HOME")
-                  "~/.dotfiles/share")
-              "emacs"))
-
-(load (format "%s/%s" user-emacs-directory "init"))
