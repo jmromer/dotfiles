771f4f42ef202499187d6f4e716974af05881679
Jake Romer <jake@jmromer.org>
2022-09-02 00:44:24 -0400

    Add safe-local vars

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((lsp-html-format-enable)))
+ '(safe-local-variable-values
+   '((rubocop-check-command . "bundle exec rubocop --format emacs")
+     (rubocop-format-command . "bundle exec rubocop --format emacs -x")
+     (rubocop-autocorrect-command . "bundle exec rubocop --format emacs -a")
+     (lsp-html-format-enable)))
  '(warning-suppress-log-types '((lsp-mode) (lsp-mode) (defvaralias)))
  '(warning-suppress-types '((lsp-mode) (defvaralias))))
 (custom-set-faces
