71bf43df47f34ead22685fd865dd3c282c9ac737
Jake Romer <jake@jmromer.org>
2022-10-16 21:28:43 -0400

    Add chemacs profiles file

---
+++ config/chemacs/profiles.el
@@ -0,0 +1,3 @@
+(("default"   . ((user-emacs-directory . "~/.dotfiles/share/doomemacs")))
+ ("doom" . ((user-emacs-directory . "~/.dotfiles/share/doomemacs")))
+ ("spacemacs"   . ((user-emacs-directory . "~/.dotfiles/share/spacemacs"))))
