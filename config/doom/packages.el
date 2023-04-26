5b2b6e6441d2c5ebd8fbd86c4987aa9ede6b36d4
Jake Romer <jake@jmromer.org>
2023-04-26 12:27:28 -0500

    [emacs] Add codespaces with configuration

--- config/doom/packages.el
+++ config/doom/packages.el
@@ -8,6 +8,7 @@
 ;; API Demos:
 ;; https://github.com/hlissner/doom-emacs/blob/develop/modules/lang/emacs-lisp/demos.org
 
+(package! codespaces)
 (package! command-log-mode)
 (package! company-box)
 (package! company-lsp)
