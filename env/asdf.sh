ca5213c6bd66da29fc06ac6e0891b54bc869ef3e
Jacob Romer <jmromer@tensorconclave.com>
2023-10-31 11:04:46 -0400

    [asdf] Remove tool-versions file from xdg scheme

--- env/asdf.sh
+++ env/asdf.sh
@@ -18,7 +18,8 @@ export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/config"
 
 # The filename of the file storing the tool names and versions.
 # Defaults to .tool-versions. Can be any valid filename.
-export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${XDG_CONFIG_HOME}/asdf/tool-versions"
+# export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${XDG_CONFIG_HOME}/asdf/tool-versions"
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=".tool-versions"
 
 # Defaults to ~/.asdf - Location of the asdf scripts.
 # If you install asdf to some other directory, set this to that directory.
