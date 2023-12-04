81c824a06cf9f60e266188792b399148c18e4d80
Jacob Romer <jmromer@tensorconclave.com>
2023-12-04 12:00:57 -0600

    [emacs] Enable ESS mode for R scripts

--- config/doom/init.el
+++ config/doom/init.el
@@ -126,7 +126,7 @@
        ;; elm                ; care for a cup of TEA?
        emacs-lisp            ; drown in parentheses
        ;; erlang             ; an elegant language for a more civilized age
-       ;; ess                ; emacs speaks statistics
+       ess                   ; emacs speaks statistics
        ;; factor
        ;; faust              ; dsp, but you get to keep your soul
        ;; fsharp             ; ML stands for Microsoft's Language
@@ -159,7 +159,7 @@
        ;; raku               ; the artist formerly known as perl6
        rest                  ; Emacs as a REST client
        rst                   ; ReST in peace
-       (ruby +rails +lsp)    ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby +rails)         ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;; rust               ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;; scala              ; java, but good
        ;; (scheme +guile)    ; a fully conniving family of lisps
