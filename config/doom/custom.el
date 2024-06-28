688c5d3924a395b6630f1ab11197fcb4c3c2f9d8
Jacob Romer <jmromer@tensorconclave.com>
2024-06-28 09:25:47 -0400

    [emacs] Update custom-safe-themes

--- config/doom/custom.el
+++ config/doom/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
    '((apheleia-global-mode . -1)
