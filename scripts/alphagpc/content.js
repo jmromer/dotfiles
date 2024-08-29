bbb9587b63de83e3a594331496e2ecda16170abf
Jacob Romer <jmromer@tensorconclave.com>
2024-08-29 16:09:09 -0400

    [alphagpc] Add sequence index

--- scripts/alphagpc/content.js
+++ scripts/alphagpc/content.js
@@ -1,23 +1,48 @@
 console.log("alphagpc loading...");
 
-const addListener = () => {
-  document.querySelector("h1").addEventListener("click", (e) => {
-    const title = e.target.innerText.replace(/:/g, "");
-    const links = Array.from(
-      document.querySelectorAll('a[data-track-page="focused_lex_video_item"]'),
-    );
-    const mp4 = links.filter((e) => /mp4/i.test(e.innerText))[0];
-    const pdf = links.filter((e) => /pdf/i.test(e.innerText))[0];
-    const vtt = links.filter((e) => /vtt/i.test(e.innerText))[0];
-    const text = [
-      'wget -O "' + title + '.mp4" "' + mp4.href + '"',
-      'wget -O "' + title + '.pdf" "' + pdf.href + '"',
-      'wget -O "' + title + '.vtt" "' + vtt.href + '"',
-    ].join("\n");
-    navigator.clipboard.writeText(text);
-    console.log(text);
-  });
+const createIndex = () => {
+  // ensure all lessons are expanded
+  document
+    .querySelectorAll("h2.lesson-name button[aria-expanded='false']")
+    .forEach((e) => e.click());
+
+  // get all lesson title elements
+  const navItems = document
+    .querySelector(".item-tools-and-content-container")
+    .querySelectorAll("a .rc-NavItemName");
+
+  // create a map of lesson titles to sequence numbers
+  const sequenceNumToItem = Array.from(navItems)
+    .filter((e) => /video/i.test(e.childNodes[0].textContent))
+    .map((e) => e.childNodes[e.childNodes.length - 1].textContent)
+    .map((e, i) => [e, i.toString().padStart(2, "0")]);
+  return new Map(sequenceNumToItem);
 };
+
+function handleClick(e) {
+  const titleIndex = createIndex();
+
+  let title = e.target.innerText.replace(/:/g, "");
+  if (!/^\d+.+/.test(title)) {
+    title = `${titleIndex.get(title)}. ${title}`;
+    if (!titleIndex.has(title))
+      console.log("[WARNING] Matching title not found in index");
+  }
+
+  const links = Array.from(
+    document.querySelectorAll('a[data-track-page="focused_lex_video_item"]'),
+  );
+  const mp4 = links.filter((e) => /mp4/i.test(e.innerText))[0];
+  const pdf = links.filter((e) => /pdf/i.test(e.innerText))[0];
+  const vtt = links.filter((e) => /vtt/i.test(e.innerText))[0];
+  const command = [
+    'wget -O "' + title + '.mp4" "' + mp4.href + '"',
+    'wget -O "' + title + '.pdf" "' + pdf.href + '"',
+    'wget -O "' + title + '.vtt" "' + vtt.href + '"',
+  ].join("\n");
+  navigator.clipboard.writeText(command);
+  console.log(command);
+}
 
 const toggleDownloads = () => {
   const section =
@@ -26,16 +51,18 @@ const toggleDownloads = () => {
   if (downloadSection) downloadSection.click();
 };
 
+// TODO: prevent duplicate event listeners
 window.addEventListener("load", () => {
   console.log("alphagpc ready.");
   toggleDownloads();
-  addListener();
+  document.querySelector("h1").addEventListener("click", handleClick);
+  document.querySelector("h1").setAttribute("style", "cursor: pointer;");
 });
 
 const observer = new MutationObserver((_mutations) => {
   console.log("DOM mutation detected");
   toggleDownloads();
-  addListener();
+  document.querySelector("h1").addEventListener("click", handleClick);
+  document.querySelector("h1").setAttribute("style", "cursor: pointer;");
 });
-
 observer.observe(document.body, { childList: true, subtree: true });
