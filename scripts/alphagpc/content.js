b4d458925aa9b0620f35425069547b7066bd70ec
Jacob Romer <jmromer@tensorconclave.com>
2024-08-31 00:18:08 -0400

    [alphagpc] Add numbering fixes

--- scripts/alphagpc/content.js
+++ scripts/alphagpc/content.js
@@ -15,31 +15,38 @@ const createIndex = () => {
   const sequenceNumToItem = Array.from(navItems)
     .filter((e) => /video/i.test(e.childNodes[0].textContent))
     .map((e) => e.childNodes[e.childNodes.length - 1].textContent)
-    .map((e, i) => [e, i.toString().padStart(2, "0")]);
+    .map((e, i) => [e, (i + 1).toString().padStart(2, "0")]);
   return new Map(sequenceNumToItem);
 };
 
 function handleClick(e) {
   const titleIndex = createIndex();
 
-  let title = e.target.innerText.replace(/:/g, "");
+  let title = e.target.innerText;
   if (!/^\d+.+/.test(title)) {
-    title = `${titleIndex.get(title)}. ${title}`;
-    if (!titleIndex.has(title))
+    console.log(`[INFO] Title does not start with a sequence number.`);
+    console.log(`[INFO] Checking index for: '${title}'`);
+    if (titleIndex.has(title)) {
+      console.log("[INFO] Found matching title in index.");
+      title = `${titleIndex.get(title)}. ${title}`;
+    } else {
       console.log("[WARNING] Matching title not found in index");
+    }
   }
 
-  const links = Array.from(
-    document.querySelectorAll('a[data-track-page="focused_lex_video_item"]'),
-  );
+  const videoDownloadsCSS = 'a[data-track-page="focused_lex_video_item"]';
+  const links = Array.from(document.querySelectorAll(videoDownloadsCSS));
   const mp4 = links.filter((e) => /mp4/i.test(e.innerText))[0];
   const pdf = links.filter((e) => /pdf/i.test(e.innerText))[0];
   const vtt = links.filter((e) => /vtt/i.test(e.innerText))[0];
-  const command = [
-    'wget -O "' + title + '.mp4" "' + mp4.href + '"',
-    'wget -O "' + title + '.pdf" "' + pdf.href + '"',
-    'wget -O "' + title + '.vtt" "' + vtt.href + '"',
-  ].join("\n");
+
+  let command = [];
+  const filename = title.replace(/[\/:]/g, "_");
+  mp4 && command.push('wget -O "' + filename + '.mp4" "' + mp4.href + '"');
+  pdf && command.push('wget -O "' + filename + '.pdf" "' + pdf.href + '"');
+  vtt && command.push('wget -O "' + filename + '.vtt" "' + vtt.href + '"');
+  command = command.join("\n");
+
   navigator.clipboard.writeText(command);
   console.log(command);
 }
