1e31d4930bfc74b870385a56e2cef445109bc928
Your Name <you@example.com>
2025-03-10 01:21:58 -0400

    [emacs] Use separate daemons and clients

--- scripts/emacs-open-client.sh
+++ scripts/emacs-open-client.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
-bring_emacs_to_front(){
- osascript <<EOF
+# Generate a unique server name (using process ID and current epoch time)
+server_name="emacs_$$_$(date +%s)"
+server_socket_path="${XDG_RUNTIME_DIR}/emacs/$server_name"
+
+emacs --daemon="$server_socket_path"
+
+timeout=8
+while [ $timeout -gt 0 ]; do
+  if [ -S "$server_socket_path" ]; then
+    echo "Daemon started successfully."
+    break
+  else
+    echo "Waiting for daemon to start..."
+    sleep 1
+    timeout=$((timeout - 1))
+  fi
+done
+
+emacsclient --create-frame --no-wait --socket-name="$server_socket_path"
+
+# bring_emacs_to_front
+osascript <<EOF
  tell application "System Events"
    tell application process "Emacs"
      set frontmost to true
    end tell
  end tell
 EOF
-}
-
-emacsclient \
-  --quiet \
-  --create-frame \
-  --no-wait \
-  --alternate-editor ''
-
-bring_emacs_to_front
