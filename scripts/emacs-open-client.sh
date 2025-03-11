03c2a00e5572354b1f91b677a1cee0edf5db7f35
Jacob Romer <jmromer@tensorconclave.com>
2025-03-11 19:15:30 -0400

    [emacs] Restore simplified open-client script

--- scripts/emacs-open-client.sh
+++ scripts/emacs-open-client.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-# Generate a unique server name (using process ID and current epoch time)
-server_name="emacs_$$_$(date +%s)"
-server_socket_path="${XDG_RUNTIME_DIR}/emacs/$server_name"
+emacsclient \
+  --quiet \
+  --create-frame \
+  --no-wait \
+  --alternate-editor ''
 
-emacs --daemon="$server_socket_path"
-
-timeout=8
-while [ $timeout -gt 0 ]; do
-  if [ -S "$server_socket_path" ]; then
-    echo "Daemon started successfully."
-    break
-  else
-    echo "Waiting for daemon to start..."
-    sleep 1
-    timeout=$((timeout - 1))
-  fi
-done
-
-emacsclient --create-frame --no-wait \
-  --socket-name="$server_socket_path" \
-  --frame-parameters "((left . 0.75) (top . 0) (width . 0.4) (fullscreen . fullheight))" \
-
-# bring_emacs_to_front
+# bring emacs to front
 osascript <<EOF
- tell application "System Events"
-   tell application process "Emacs"
-     set frontmost to true
-   end tell
- end tell
+  tell application "Emacs"
+    activate
+  end tell
+
+  tell application "System Events"
+    tell application process "Emacs"
+      set frontmost to true
+    end tell
+  end tell
 EOF
