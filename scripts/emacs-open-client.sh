5b61a5696e6d30bb2a8d7a8661552aab36eb4b17
Your Name <you@example.com>
2025-03-12 11:01:02 -0400

    [emacs] Ensure emacs is foregrounded on open

--- scripts/emacs-open-client.sh
+++ scripts/emacs-open-client.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+debug(){
+ echo "[EMACS] ${1}"
+ syslog -s -l notice "[EMACS] ${1}"
+}
+
+bring_emacs_to_front () {
+  debug "Bringing Emacs to front"
+
+  osascript <<EOF
+    tell application "Emacs"
+      activate
+    end tell
+EOF
+
+  osascript <<EOF
+    tell application "System Events"
+      tell application process "Emacs"
+        set frontmost to true
+      end tell
+    end tell
+EOF
+}
+
 emacsclient \
   --quiet \
   --create-frame \
   --no-wait \
   --alternate-editor ''
 
-# bring emacs to front
-osascript <<EOF
-  tell application "Emacs"
-    activate
-  end tell
-
-  tell application "System Events"
-    tell application process "Emacs"
-      set frontmost to true
-    end tell
-  end tell
-EOF
+bring_emacs_to_front
