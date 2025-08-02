80d460358b7b36967743b52ef63579253aa78706
Jacob Romer <jmromer@tensorconclave.com>
2025-08-02 15:47:53 -0400

    [zshenv] Disable old workarounds

--- env/path.sh
+++ env/path.sh
@@ -5,7 +5,6 @@
 PATH="${XDG_LOCALS_DIR}/bin"
 PATH+=":${DOTFILES_DIR}/bin"
 PATH+=":${XDG_SECURE_DIR}/bin"
-PATH+=":./node_modules/.bin"
 
 PATH+=":${XDG_DATA_HOME}/mise/shims"
 PATH+=":${XDG_DATA_HOME}/fzf/bin"
@@ -13,14 +12,10 @@ PATH+=":${XDG_DATA_HOME}/git/bin"
 PATH+=":${XDG_DATA_HOME}/doomemacs/bin"
 PATH+=":${XDG_DATA_HOME}/npm/bin"
 
-PATH+=":${HOMEBREW_PREFIX}/opt/${OPENSSL:-openssl@1.1}/bin"
-PATH+=":${HOMEBREW_PREFIX}/opt/libpq/bin"
 PATH+=":${HOMEBREW_PREFIX}/bin"
 PATH+=":${HOMEBREW_PREFIX}/sbin"
-PATH+=":/usr/local/bin" # some binaries use /usr/local/bin on Apple silicon
-
-PATH+=":${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/bison/bin"
+PATH+=":${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/curl/bin"
 PATH+=":${HOMEBREW_PREFIX}/opt/findutils/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/gettext/bin"
@@ -33,24 +28,29 @@ PATH+=":${HOMEBREW_PREFIX}/opt/gnutls/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/grep/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/make/libexec/gnubin"
 PATH+=":${HOMEBREW_PREFIX}/opt/rg/bin"
-
 PATH+=":${HOMEBREW_PREFIX}/share/git-core/contrib/diff-highlight"
 
 PATH+=":/usr/bin"
 PATH+=":/bin"
 PATH+=":/usr/sbin"
 PATH+=":/sbin"
-
-PATH+=":${HOME}/.docker/bin"
 PATH+=":/Library/TeX/texbin"
-export PATH
 
-# Prefer BSD uname, becuase it reports m1 arch as 'arm'
-# GNU uname reports it as 'arm64', which breaks some build scripts
-if [[ -d "${HOMEBREW_PREFIX}/bin" ]]; then
-  ln -sf /usr/bin/uname "${HOMEBREW_PREFIX}/bin/uname"
-fi
+export PATH
 
 for mandir in ${HOMEBREW_PREFIX}/opt/*/libexec/gnuman; do
   export MANPATH="$mandir:$MANPATH"
 done
+
+# TEMP: Let's see what breaks
+# PATH+=":./node_modules/.bin"
+# PATH+=":${HOMEBREW_PREFIX}/opt/${OPENSSL:-openssl@1.1}/bin"
+# PATH+=":${HOMEBREW_PREFIX}/opt/libpq/bin"
+# PATH+=":/usr/local/bin" # some binaries use /usr/local/bin on Apple silicon
+# PATH+=":${HOME}/.docker/bin"
+#
+# Prefer BSD uname, becuase it reports m1 arch as 'arm'
+# GNU uname reports it as 'arm64', which breaks some build scripts
+# if [[ -d "${HOMEBREW_PREFIX}/bin" ]]; then
+#   ln -sf /usr/bin/uname "${HOMEBREW_PREFIX}/bin/uname"
+# fi
