df81f4102a7df7d251ec2321df59251bfd0e8c17
Jacob Romer <jmromer@tensorconclave.com>
2025-08-06 15:35:37 -0400

    [k8s] Disable aliases

--- env/kubernetes.sh
+++ env/kubernetes.sh
@@ -1,13 +1,13 @@
 alias k=kubectl
 
-if [[ -n "${ZSH_VERSION}" ]]; then
-  alias -g now='--force --grace-period=0'
-  alias -g cfg='--dry-run=client --output=yaml'
-elif [[ -n "${BASH_VERSION}" ]]; then
-  export now='--force --grace-period=0'
-  export cfg='--dry-run=client --output=yaml'
-fi
+# if [[ -n "${ZSH_VERSION}" ]]; then
+#   alias -g now='--force --grace-period=0'
+#   alias -g cfg='--dry-run=client --output=yaml'
+# elif [[ -n "${BASH_VERSION}" ]]; then
+#   export now='--force --grace-period=0'
+#   export cfg='--dry-run=client --output=yaml'
+# fi
 
-function req() {
-  kubectl run tmp --image=nginx:alpine --rm -i --restart=Never -- curl "${1}"
-}
+# function req() {
+#   kubectl run tmp --image=nginx:alpine --rm -i --restart=Never -- curl "${1}"
+# }
