0cc15c5aec331b502959a6bb8db6cccde510dfc5
Jacob Romer <jmromer@tensorconclave.com>
2025-08-19 09:57:33 -0400

    [alphagpc] Update for ui changes

--- scripts/alphagpc/coursera-content.js
+++ scripts/alphagpc/coursera-content.js
@@ -1,13 +1,11 @@
 const createIndex = () => {
-  // get all lesson title elements
-  const navItems = document
-    .querySelector(".item-tools-and-content-container")
-    .querySelectorAll("a .rc-NavItemName");
+  const navPanel = document.querySelector("[aria-label=Close]").closest(".cds-grid-item");
+  const navItems = [...navPanel.querySelectorAll(".outline-single-item-content-wrapper")].map(e => e.querySelectorAll("div")[1]);
 
   // create a map of lesson titles to sequence numbers
   const sequenceNumToItem = Array.from(navItems)
-    .filter((e) => /video/i.test(e.childNodes[0].textContent))
-    .map((e) => e.childNodes[e.childNodes.length - 1].textContent)
+    .filter((e) => /video/i.test(e.childNodes[1].textContent))
+    .map((e) => e.childNodes[0].textContent)
     .map((e, i) => [e, (i + 1).toString().padStart(2, "0")]);
   return new Map(sequenceNumToItem);
 };
@@ -15,10 +13,13 @@ const createIndex = () => {
 function generateDownloadCommand(e) {
   const titleIndex = createIndex();
   const WEEKNUM = document
-    .querySelector('[data-track-component="item_nav_week_number"]')
-    .textContent.split(" ")
-    .at(1)
-    .padStart(2, "0");
+        .querySelector('[data-current-item=true]')
+        .closest('.cds-AccordionRoot-container')
+        .querySelector('.cds-AccordionHeader-labelGroup div')
+        .textContent
+        .split(" ")
+        .at(1)
+        .padStart(2, "0");
 
   const button = e.target;
   const header = button.previousElementSibling;
@@ -47,10 +48,10 @@ function generateDownloadCommand(e) {
 
   let command = [];
   const filename = title.replace(/\//g, "-").replace(/:/g, " -");
-  mp4 && command.push('wget -qO "' + filename + '.mp4" "' + mp4.href + '" &');
-  pdf && command.push('wget -qO "' + filename + '.pdf" "' + pdf.href + '" &');
-  vtt && command.push('wget -qO "' + filename + '.vtt" "' + vtt.href + '" &');
-  txt && command.push('wget -qO "' + filename + '.txt" "' + txt.href + '" &');
+  mp4 && command.push('wget -qO "module' + WEEKNUM + '/' + filename + '.mp4" "' + mp4.href + '" &');
+  pdf && command.push('wget -qO "module' + WEEKNUM + '/' + filename + '.pdf" "' + pdf.href + '" &');
+  vtt && command.push('wget -qO "module' + WEEKNUM + '/' + filename + '.vtt" "' + vtt.href + '" &');
+  txt && command.push('wget -qO "module' + WEEKNUM + '/' + filename + '.txt" "' + txt.href + '" &');
 
   return command.join("\n");
 }
@@ -58,7 +59,7 @@ function generateDownloadCommand(e) {
 function handleClick(e) {
   // ensure all lessons are expanded
   document
-    .querySelectorAll("h2.lesson-name button[aria-expanded='false']")
+    .querySelectorAll("h3 button[aria-expanded=false]")
     .forEach((e) => e.click());
 
   const observer = new MutationObserver(async (mutations, obs) => {
@@ -70,10 +71,11 @@ function handleClick(e) {
     const script = /wget/.test(clipboardText)
       ? [clipboardText, command].join("\n")
       : command;
+    console.log(script);
     navigator.clipboard.writeText(script);
 
     // Advance to next page
-    const nextButton = document.querySelector('button[aria-label="Next Item"]');
+    const nextButton = document.querySelector('button[aria-label="Go to next item"]');
     if (nextButton) {
       nextButton.click();
     }
@@ -84,7 +86,7 @@ function handleClick(e) {
 }
 
 const toggleDownloads = () => {
-  const section = 'button[data-testid="lecture-downloads-tab"]';
+  const section = "button[data-testid=lecture-downloads-tab]";
   const downloadSection = document.querySelector(section);
   if (downloadSection) downloadSection.click();
 };
@@ -92,7 +94,7 @@ const toggleDownloads = () => {
 function addButtonContent() {
     toggleDownloads();
     const btnHtml = `
-      <button id="download-content" class="css-1qi5els" type="button">
+      <button id="download-content" class="css-1md39p1" type="button">
         Download
       </button>`;
     const headerEl = document.querySelector('h1');
